a0b47cfcd84081a2102d21143dc6c52b9836f6b5
varun1505 <varun1505@gmail.com>
2016-01-03 16:53:18 +0530

    random images from unsplash loaded as texture for tiles

--- js/script.js
+++ js/script.js
@@ -18,12 +18,17 @@ var light = new THREE.AmbientLight( 0xf0f0f0 );
 scene.add(light);
 
 var createTile = function(x, y, z, imageUrl, link) {
-  var geometry = new t3.PlaneGeometry(1, 1);
-  var material = new t3.MeshLambertMaterial({color: 'red'});
-  var tile = new t3.Mesh(geometry, material);
-  tile.position.set(x, y, z);    
-  tile.lookAt(new t3.Vector3(0,0,0));
-  scene.add(tile);
+  	var geometry = new t3.PlaneGeometry(1, 1);
+	// var material = new t3.MeshLambertMaterial({color: 'red'});
+
+	THREE.ImageUtils.crossOrigin = '';
+	var texture = THREE.ImageUtils.loadTexture('https://unsplash.it/128/128/?random&time=' + new Date().getTime() + Math.random());
+	
+	var material = new t3.MeshBasicMaterial({map: texture});
+	var tile = new t3.Mesh(geometry, material);
+	tile.position.set(x, y, z);
+	tile.lookAt(new t3.Vector3(0,0,0));
+	scene.add(tile);
 }
 
 var noOfTiles = 100;
@@ -31,27 +36,27 @@ var radius = 6;
 
 for( var i = 0 ; i < noOfTiles; i++) {
   
-  var angle = 15 * i * Math.PI / 180;
-  
-  var phi = Math.acos( -1 + ( 2 * i ) / noOfTiles );
-  var theta = Math.sqrt( noOfTiles * Math.PI ) * phi;
+	var angle = 15 * i * Math.PI / 180;
 
-  var x = radius * Math.cos( theta ) * Math.sin( phi );
-  var y = radius * Math.sin( theta ) * Math.sin( phi );
-  var z = radius * Math.cos( phi );
+	var phi = Math.acos( -1 + ( 2 * i ) / noOfTiles );
+	var theta = Math.sqrt( noOfTiles * Math.PI ) * phi;
 
-  /*var x = 5 * Math.sin(angle);
-  var z = 5 * Math.cos(angle);*/
+	var x = radius * Math.cos( theta ) * Math.sin( phi );
+	var y = radius * Math.sin( theta ) * Math.sin( phi );
+	var z = radius * Math.cos( phi );
 
-  // https://unsplash.it/200/200/?random
+	/*var x = 5 * Math.sin(angle);
+	var z = 5 * Math.cos(angle);*/
 
-  createTile(x, y, z, '', '');
+	// 
+
+	createTile(x, y, z, '', '');
 
 }
 
 function render() {
-  requestAnimationFrame(render); 
-  controls.update();
-  manager.render(scene, camera);
+	requestAnimationFrame(render); 
+	controls.update();
+	manager.render(scene, camera);
 }
 render();
