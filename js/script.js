a7019b2ccf2b131102828882768ae062706cd4df
varun1505 <varun1505@gmail.com>
2016-01-03 15:49:56 +0530

    removed skybox. other code cleanup

--- js/script.js
+++ js/script.js
@@ -55,47 +55,6 @@ for( var i = 0 ; i< l; i++) {
 }
 
 
-// Add a repeating grid as a skybox.
-var boxWidth = 50;
-var loader = new THREE.TextureLoader();
-loader.load('img/box.png', onTextureLoaded);
-
-function onTextureLoaded(texture) {
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set(boxWidth, boxWidth);
-
-  var geometry = new THREE.BoxGeometry(boxWidth, boxWidth, boxWidth);
-  var material = new THREE.MeshBasicMaterial({
-    map: texture,
-    color: 0xFFFFFF,
-    side: THREE.BackSide
-  });
-
-  var skybox = new THREE.Mesh(geometry, material);
-  scene.add(skybox);
-}
-
-
-
-/*var makeCube = function(x, y, z) {
-  var cube = new t3.Mesh(geometry, material);  
-  cube.position.set(-6, -10, -5).add(new t3.Vector3(x, y, z));  
-  scene.add(cube);
-  return cube;
-}
-
-var rows, cols, spacing = 1.3, cubes = [];
-
-rows = cols = 9;
-
-for( var r = 0; r < rows; r++ ) {
-  for ( var c = 0; c < cols; c++ ) {
-    if(c == 0) {cubes[r] = []}
-    // cubes[r][c] = makeCube(r * spacing, 0, c * spacing * -1);
-  }  
-}*/
-
 
 camera.position.y = 0;
 camera.position.z = 0;
@@ -104,13 +63,7 @@ camera.position.z = 0;
 var cubeYPos = 0;
 
 function render() {
-  requestAnimationFrame(render);
-  /*cubeYPos += 0.01;
-  for( var r = 0; r < rows; r++ ) {
-    for ( var c = 0; c < cols; c++ ) {
-      //cubes[r][c].position.setY(Math.sin(cubeYPos) * Math.PI );
-    }  
-  }*/
+  requestAnimationFrame(render); 
 
   controls.update();
 
