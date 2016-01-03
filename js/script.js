6d8d16767755f4948ef67a4fcf2eb1a0fa7bde5e
varun1505 <varun1505@gmail.com>
2016-01-03 16:21:04 +0530

    code restructured. method to create tile added

--- js/script.js
+++ js/script.js
@@ -14,25 +14,27 @@ var effect = new THREE.VREffect(renderer);
 var manager = new WebVRManager(renderer, effect);
 var controls = new THREE.VRControls(camera);
 
-var geometry = new t3.PlaneGeometry(1, 1);
-var material = new t3.MeshLambertMaterial({color: 'red'});
-
-// var light = new t3.HemisphereLight( 0xffffff, 0x080820, 1 );
 var light = new THREE.AmbientLight( 0xf0f0f0 );
 scene.add(light);
 
-var l = 100;
+var createTile = function(x, y, z, imageUrl, link) {
+  var geometry = new t3.PlaneGeometry(1, 1);
+  var material = new t3.MeshLambertMaterial({color: 'red'});
+  var tile = new t3.Mesh(geometry, material);
+  tile.position.set(x, y, z);    
+  tile.lookAt(new t3.Vector3(0,0,0));
+  scene.add(tile);
+}
 
-var radius = 5;
+var noOfTiles = 100;
+var radius = 6;
 
-for( var i = 0 ; i< l; i++) {
+for( var i = 0 ; i < noOfTiles; i++) {
   
   var angle = 15 * i * Math.PI / 180;
   
-  var phi = Math.acos( -1 + ( 2 * i ) / l );
-  var theta = Math.sqrt( l * Math.PI ) * phi;
-
-
+  var phi = Math.acos( -1 + ( 2 * i ) / noOfTiles );
+  var theta = Math.sqrt( noOfTiles * Math.PI ) * phi;
 
   var x = radius * Math.cos( theta ) * Math.sin( phi );
   var y = radius * Math.sin( theta ) * Math.sin( phi );
@@ -41,35 +43,15 @@ for( var i = 0 ; i< l; i++) {
   /*var x = 5 * Math.sin(angle);
   var z = 5 * Math.cos(angle);*/
 
-  var vector = new t3.Vector3();
+  // https://unsplash.it/200/200/?random
 
-  // for (var y = -4; y <= 4; y = y + 2) {
-    var cube = new t3.Mesh(geometry, material);
-    cube.position.set(x, y, z);
-    var cubeContainer = new t3.Object3D();
-    cubeContainer.add(cube);
+  createTile(x, y, z, '', '');
 
-    vector.copy( cube.position ).multiplyScalar( 2 );
-    cube.lookAt(new t3.Vector3(0,0,0));
-    scene.add(cube);
-  // }
 }
-
-
-
-camera.position.y = 0;
-camera.position.z = 0;
-
-
-var cubeYPos = 0;
 
 function render() {
   requestAnimationFrame(render); 
-
   controls.update();
-
   manager.render(scene, camera);
 }
-
-
 render();
